497514156eb06079c1fb5d31658c9c101f9b1faf
ains1829 <andyrakotonavalona0@gmail.com>
2024-12-10 16:14:39 +0300

    update sql

--- sql/Sql.sql
+++ sql/Sql.sql
@@ -1,8 +1,8 @@
 create database login_system;
 
 create table users (
-  user_id serial primary key,          --   Id unique de l'utilisateur
-  email varchar(255) NOT NULL UNIQUE,        -- Nom d'utilisateur
-  password_hash varchar(255),            -- mot de passe
-  registration_date::timestamp(6) without time zone          -- Date d'inscription
+  user_id serial primary key,                        -- Id unique de l'utilisateur
+  email varchar(255) NOT NULL UNIQUE,                -- email 'utilisateur
+  password_hash varchar(255),                        -- mot de passe
+  registration_date::timestamp(6) without time zone  -- Date d'inscription
 );
